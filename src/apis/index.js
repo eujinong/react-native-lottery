4a5eec6bfdb8b67beba2a5b934cac09902bc9be9
nickjang1 <eunhyukjang1@gmail.com>
2018-09-03 23:01:38 +0700

    first

---
+++ src/apis/index.js
@@ -0,0 +1,82 @@
+import CONFIG from '@src/config';
+import AppHelper from '@helpers/AppHelper';
+
+const Api = {
+  async getDomains() {
+    const url = `${CONFIG.SERVER}/config`;
+    try {
+      const res = await fetch(url);
+      if (res.status === 200) {
+        const data = await res.json();
+        return data;
+      }
+      return null;
+    } catch (error) {
+      console.log(error); return null;
+    }
+  },
+
+  async getGameResult($params) {
+    const query = AppHelper.buildHttpQuery($params);
+    const url = `${CONFIG.SERVER}/sessions?${query}`;
+    try {
+      const res = await fetch(url);
+      if (res.status === 200) {
+        const data = await res.json();
+        return data;
+      }
+      return null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+
+  async getCompanyResult($params) {
+    const query = AppHelper.buildHttpQuery($params);
+    const url = `${CONFIG.SERVER}/company-sessions?${query}`;
+    try {
+      const res = await fetch(url);
+      if (res.status === 200) {
+        const data = await res.json();
+        return data;
+      }
+      return null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+
+  async getSearch(query) {
+    const url = `${CONFIG.SERVER}/search?${query}`;
+    try {
+      const res = await fetch(url);
+      if (res.status === 200) {
+        const data = await res.json();
+        return data;
+      }
+      return null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+
+  async getStatResult(param) {
+    const url = `${CONFIG.SERVER}/${param}`;
+    try {
+      const res = await fetch(url);
+      if (res.status === 200) {
+        const data = await res.json();
+        return data;
+      }
+      return null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+};
+
+export default Api;
---
+++ src/theme/Images.js
@@ -0,0 +1,12 @@
+const Images = {
+  splash: require('../../../images/splash.png'),
+  logo: require('../../assets/images/logo.png'),
+  icon_logo: require('@assets/images/icon_logo.png'),
+  icon_calendar: require('@assets/images/icon_calendar.png'),
+  icon_cold: require('@assets/images/icon_cold.png'),
+  icon_graph: require('@assets/images/icon_graph.png'),
+  icon_hot: require('@assets/images/icon_hot.png'),
+  icon_search: require('@assets/images/icon_search.png'),
+};
+
+export default Images;

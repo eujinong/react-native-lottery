190fd308dff5df3269b9aefe9da56521c8f0a821
nickjang1 <eunhyukjang1@gmail.com>
2018-09-06 05:16:28 +0700

    firebase activate

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'react-native-firebase';
+
+const { Banner, AdRequest } = firebase.admob;
+const request = new AdRequest();
 
 class Index extends Component {
   render() {
     return (
-      <View />
+      <View>
+        <Banner
+          unitId="ca-app-pub-3940256099942544/2934735716"
+          request={request.build()}
+          onAdLoaded={() => {
+            console.log('Advert loaded');
+          }}
+        />
+      </View>
     );
   }
 }
